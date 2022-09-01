27068ed78638cc525e67728db92e439b20e53c95
hangunhee <ghee0831@naver.com>
2022-09-01 20:21:34 +0900

    start

---
+++ movie_grade/app/src/main/java/hgh/project/movie_grade/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package hgh.project.movie_grade.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
